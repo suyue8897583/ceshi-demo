5d5006fb7ae2c5968913dde40d6e9785d3f8d963
苏钺 <545681450@qq.com>
2020-12-22 15:25:34 +0800

    测试

--- pages/contact/contact.vue
+++ pages/contact/contact.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-04-19 00:56:28
- * @LastEditTime: 2020-12-22 15:17:40
+ * @LastEditTime: 2020-12-22 15:24:48
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \hm_shop\pages\contact\contact.vue
@@ -11,7 +11,7 @@
 		<image class="img" src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2483518927,608217211&fm=26&gp=0.jpg"></image>
 		<view class="info">
 			<view @click="phone">联系电话：400-618-9090 ( 点击拨打 )</view>
-			<view>校区地址：浙江省杭州市下沙经济开发区4号大街187号盛泰时代山都</view>
+			<view>校区地址：来了浙江省杭州市下沙经济开发区4号大街187号盛泰时代山都</view>
 		</view>
 		<map class="map" :longitude="longitude" :scale="scale" :latitude="latitude" :markers="markers"></map>
 	</view>
